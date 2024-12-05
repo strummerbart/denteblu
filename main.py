f44cdef4c442a185d169ed81d7d556cd53661850
strummerbart <103561619+strummerbart@users.noreply.github.com>
2024-12-05 15:56:54 +0100

    Modifiche varie

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ def on_bluetooth_connected():
 bluetooth.on_bluetooth_connected(on_bluetooth_connected)
 
 def on_bluetooth_disconnected():
+    global connected
     basic.show_icon(IconNames.SQUARE)
+    connected = 1
 bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
 
 def on_button_pressed_a():
-    bluetooth.uart_write_string("Hi")
+    if connected == 1:
+        bluetooth.uart_write_string("Ciao da " + control.device_name())
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_uart_data_received():
-    basic.show_icon(IconNames.HEART)
+    global uartDataReceived
+    if connected == 1:
+        uartDataReceived = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+        basic.clear_screen()
+        basic.show_arrow(ArrowNames.EAST)
+        basic.pause(2000)
+        basic.show_string(uartDataReceived)
 bluetooth.on_uart_data_received(serial.delimiters(Delimiters.NEW_LINE),
     on_uart_data_received)
 
+uartDataReceived = ""
+connected = 0
 bluetooth.start_uart_service()
-basic.show_string("Tested with http://bit.ly/serial-bluetooth")
+basic.show_string("UART")
 basic.show_icon(IconNames.SQUARE)
--- main.ts
+++ main.ts
@@ -1,15 +1,30 @@
-bluetooth.onBluetoothConnected(function () {
+bluetooth.onBluetoothConnected(function on_bluetooth_connected() {
     basic.showIcon(IconNames.SmallSquare)
 })
-bluetooth.onBluetoothDisconnected(function () {
+bluetooth.onBluetoothDisconnected(function on_bluetooth_disconnected() {
+    
     basic.showIcon(IconNames.Square)
+    connected = 1
 })
-input.onButtonPressed(Button.A, function () {
-    bluetooth.uartWriteString("Hi")
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (connected == 1) {
+        bluetooth.uartWriteString("Ciao da " + control.deviceName())
+    }
+    
 })
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    basic.showIcon(IconNames.Heart)
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_uart_data_received() {
+    
+    if (connected == 1) {
+        uartDataReceived = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+        basic.clearScreen()
+        basic.showArrow(ArrowNames.East)
+        basic.pause(2000)
+        basic.showString(uartDataReceived)
+    }
+    
 })
+let uartDataReceived = ""
+let connected = 0
 bluetooth.startUartService()
-basic.showString("Tested with http://bit.ly/serial-bluetooth")
+basic.showString("UART")
 basic.showIcon(IconNames.Square)
