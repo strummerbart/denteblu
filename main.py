ee46e750b685e77009619ffc87bfb17670ee5701
strummerbart <103561619+strummerbart@users.noreply.github.com>
2024-12-06 10:49:01 +0100

    Rimossa variabile connected

--- main.py
+++ main.py
@@ -3,32 +3,27 @@ def on_bluetooth_connected():
 bluetooth.on_bluetooth_connected(on_bluetooth_connected)
 
 def on_bluetooth_disconnected():
-    global connected
     basic.show_icon(IconNames.SQUARE)
-    connected = 1
 bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
 
-def on_button_pressed_a():
-    if connected == 1:
-        bluetooth.uart_write_string("Ciao da " + control.device_name())
-        basic.clear_screen()
-        basic.show_icon(IconNames.SMALL_SQUARE)        
+def on_button_pressed_a():    
+    bluetooth.uart_write_string("Ciao da " + control.device_name())
+    basic.clear_screen()
+    basic.show_icon(IconNames.SMALL_SQUARE)        
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_uart_data_received():
-    global uartDataReceived
-    if connected == 1:
-        uartDataReceived = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
-        basic.clear_screen()
-        basic.show_arrow(ArrowNames.EAST)
-        basic.pause(2000)
-        basic.clear_screen()
-        basic.show_string(uartDataReceived)
+    global uartDataReceived    
+    uartDataReceived = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+    basic.clear_screen()
+    basic.show_arrow(ArrowNames.EAST)
+    basic.pause(2000)
+    basic.clear_screen()
+    basic.show_string(uartDataReceived)
 bluetooth.on_uart_data_received(serial.delimiters(Delimiters.NEW_LINE),
     on_uart_data_received)
 
 uartDataReceived = ""
-connected = 0
 bluetooth.start_uart_service()
 basic.show_string("UART")
 basic.show_icon(IconNames.SQUARE)
--- main.ts
+++ main.ts
@@ -2,32 +2,23 @@ bluetooth.onBluetoothConnected(function on_bluetooth_connected() {
     basic.showIcon(IconNames.SmallSquare)
 })
 bluetooth.onBluetoothDisconnected(function on_bluetooth_disconnected() {
-    
     basic.showIcon(IconNames.Square)
-    connected = 1
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    if (connected == 1) {
-        bluetooth.uartWriteString("Ciao da " + control.deviceName())
-        basic.clearScreen()
-        basic.showIcon(IconNames.SmallSquare)
-    }
-    
+    bluetooth.uartWriteString("Ciao da " + control.deviceName())
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallSquare)
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_uart_data_received() {
     
-    if (connected == 1) {
-        uartDataReceived = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-        basic.clearScreen()
-        basic.showArrow(ArrowNames.East)
-        basic.pause(2000)
-        basic.clearScreen()
-        basic.showString(uartDataReceived)
-    }
-    
+    uartDataReceived = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.East)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.showString(uartDataReceived)
 })
 let uartDataReceived = ""
-let connected = 0
 bluetooth.startUartService()
 basic.showString("UART")
 basic.showIcon(IconNames.Square)
